e5aef03fe0100264a3ef4951b8aa8592972e2d28
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-10-08 17:03:11 +0800

    Add function to do manual code inputs

--- src/api/voucher.ts
+++ src/api/voucher.ts
@@ -1,6 +1,6 @@
-import { getFileParts } from "utils/file"
-import request from "./request"
-import useRequest, { Config } from "./swr"
+import { getFileParts } from "utils/file";
+import request from "./request";
+import useRequest, { Config } from "./swr";
 
 export const useVouchers = (
   email: string,
@@ -9,95 +9,104 @@ export const useVouchers = (
   useRequest<{ data: CodeByEmail[] }>(
     { method: "GET", url: `voucher/${email}/getCodeByEmails/` },
     config
-  )
+  );
 
 export const useDynamicVouchers = (
   email: string,
-  config?: Config<{ unredeemed: DynamicCode[], redeemed: string[] }>
+  config?: Config<{ unredeemed: DynamicCode[]; redeemed: string[] }>
 ) =>
-  useRequest<{ unredeemed: DynamicCode[],  redeemed: string[] }>(
+  useRequest<{ unredeemed: DynamicCode[]; redeemed: string[] }>(
     { method: "GET", url: `voucher/getDynamicVoucher/${email}/` },
     config
-  )
+  );
 
 export const useVoucher = (voucherID: number, config?: Config<Voucher>) =>
-  useRequest<Voucher>({ method: "GET", url: `voucher/${voucherID}` }, config)
+  useRequest<Voucher>({ method: "GET", url: `voucher/${voucherID}` }, config);
 
 export const createVoucher = (
   data: Partial<PostAdminVoucher>,
   files: AdminVoucherFiles = {}
 ) => {
-  const formData = new FormData()
+  const formData = new FormData();
   Object.entries(data).forEach(([field, value]) => {
-    formData.append(field, value.toString())
-  })
+    formData.append(field, value.toString());
+  });
 
-  const { image } = files
+  const { image } = files;
   if (image) {
-    const imageFile = getFileParts(image)
-    if (imageFile) formData.append("image", imageFile, imageFile.name)
+    const imageFile = getFileParts(image);
+    if (imageFile) formData.append("image", imageFile, imageFile.name);
   }
 
-  request.post("/voucher/", formData)
-}
+  request.post("/voucher/", formData);
+};
 
 export const editVoucher = (
   uuid: string,
   data: Partial<PostAdminVoucher>,
   files: AdminVoucherFiles = {}
 ) => {
-  const formData = new FormData()
+  const formData = new FormData();
   Object.entries(data).forEach(([field, value]) => {
-    formData.append(field, value.toString())
-  })
+    formData.append(field, value.toString());
+  });
 
-  const { image } = files
+  const { image } = files;
   if (image) {
-    const imageFile = getFileParts(image)
-    if (imageFile) formData.append("image", imageFile, imageFile.name)
+    const imageFile = getFileParts(image);
+    if (imageFile) formData.append("image", imageFile, imageFile.name);
   }
 
-  return request.patch(`/voucher/${uuid}`, formData)
-}
+  return request.patch(`/voucher/${uuid}`, formData);
+};
 
 export const uploadCodeList = (
   uuid: string,
   files: Omit<AdminVoucherFiles, "image" | "email_list"> = {}
 ) => {
-  const formData = new FormData()
-  formData.append("uuid", uuid)
+  const formData = new FormData();
+  formData.append("uuid", uuid);
 
-  const { code_list } = files
+  const { code_list } = files;
 
-  const codeListFile = getFileParts(code_list!)
+  const codeListFile = getFileParts(code_list!);
   if (codeListFile)
-    formData.append("code_list", codeListFile, codeListFile.name)
+    formData.append("code_list", codeListFile, codeListFile.name);
 
-  return request.post("voucher/code-list", formData)
-}
+  return request.post("voucher/code-list", formData);
+};
 
 export const uploadEmailList = (
   uuid: string,
   files: Omit<AdminVoucherFiles, "image" | "code_list"> = {}
 ) => {
-  const formData = new FormData()
-  formData.append("uuid", uuid)
+  const formData = new FormData();
+  formData.append("uuid", uuid);
 
-  const { email_list } = files
+  const { email_list } = files;
 
-  const emailListFile = getFileParts(email_list!)
+  const emailListFile = getFileParts(email_list!);
   if (emailListFile)
-    formData.append("email_list", emailListFile, emailListFile.name)
+    formData.append("email_list", emailListFile, emailListFile.name);
 
-  request.post("voucher/email-list", formData)
-}
+  request.post("voucher/email-list", formData);
+};
 
-export const redeemVoucher = ( 
-  data: Partial<RedeemableVoucher>
-) => {
-  const formData = new FormData()
+export const uploadManualCodes = (uuid: string, data: CodeEmailInput[]) => {
+  const formData = new FormData();
+
+  formData.append("uuid", uuid);
+  data.forEach(({ key, value }) => {
+    formData.append(key, value.toString());
+  });
+
+  return request.post("voucher/codes/manual", formData);
+};
+
+export const redeemVoucher = (data: Partial<RedeemableVoucher>) => {
+  const formData = new FormData();
   Object.entries(data).forEach(([field, value]) => {
-    formData.append(field, value.toString())
-  })
-  return request.post("voucher/assignCodes/", formData)
-}
+    formData.append(field, value.toString());
+  });
+  return request.post("voucher/assignCodes/", formData);
+};
--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,82 +1,87 @@
 // Types for APIs
 
 interface Token {
-  access: string
-  refresh: string
+  access: string;
+  refresh: string;
 }
 
 interface Voucher {
-  uuid: string
-  posted_date: string
-  available_date: string
-  expiry_date: string
-  name: string
-  organization: string
-  voucher_type: string
-  description: string
-  counter: number
-  image: string
-  code_uploaded: boolean
+  uuid: string;
+  posted_date: string;
+  available_date: string;
+  expiry_date: string;
+  name: string;
+  organization: string;
+  voucher_type: string;
+  description: string;
+  counter: number;
+  image: string;
+  code_uploaded: boolean;
 }
 
 interface CodeByEmail {
-  id: number
-  voucher_id: number
-  code_id: number
-  email_id: number
+  id: number;
+  voucher_id: number;
+  code_id: number;
+  email_id: number;
+}
+
+interface CodeEmailInput {
+  key: string;
+  value: string;
 }
 
 interface DynamicCode {
-  id: string
-  uuid: number
-  code_id: number
+  id: string;
+  uuid: number;
+  code_id: number;
 }
 
 interface RedeemableVoucher {
-  id?: string
-  email: string
+  id?: string;
+  email: string;
 }
 
 type AdminVoucherFiles = {
-  image?: string
-  code_list?: string
-  email_list?: string
-}
+  image?: string;
+  code_list?: string;
+  email_list?: string;
+};
 interface AdminVoucher extends AdminVoucherFiles {
-  uuid: string
-  posted_date: string
-  available_date: string
-  expiry_date: string
-  name: string
-  voucher_type: string
-  description?: string
-  counter: number
-  image: string
-  code_uploaded?: boolean
-  organization: string
+  uuid: string;
+  posted_date: string;
+  available_date: string;
+  expiry_date: string;
+  name: string;
+  voucher_type: string;
+  description?: string;
+  counter: number;
+  image: string;
+  code_uploaded?: boolean;
+  organization: string;
 }
 
-type PostAdminVoucher = Omit<AdminVoucher, "uuid" | AdminVoucherFiles>
+type PostAdminVoucher = Omit<AdminVoucher, "uuid" | AdminVoucherFiles>;
 
 interface OrganizationVouchers {
-  count: number
-  next: string
-  previous: string
-  results: AdminVoucher[]
+  count: number;
+  next: string;
+  previous: string;
+  results: AdminVoucher[];
 }
 
 type Option = {
-  value: string | number
-  label: string
-}
+  value: string | number;
+  label: string;
+};
 
-type VoucherType = "Default" | "No code" | "Dinamically allocated"
+type VoucherType = "Default" | "No code" | "Dinamically allocated";
 
 interface User {
-  username: string
+  username: string;
 }
 
 interface Organization {
-  name: string
-  is_first_time_login: boolean
+  name: string;
+  is_first_time_login: boolean;
 }
