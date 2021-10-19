7a77da3193da1f175f99ff9c95fa48a54c19a558
Chong Jing Quan <jingquan0302@gmail.com>
2021-10-20 02:13:59 +0800

    Fix expired date bug

--- src/constants/date.ts
+++ src/constants/date.ts
@@ -1,4 +1,4 @@
 export const DATE_FORMAT = "yyyy-MM-dd HH:mm";
 export const DATE_DISPLAY_FORMAT = "dd/MM/yyyy";
 export const DATE_LONG_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-export const EXPIRED_DATE_FORMAT = "YYYY-MM-DDTHH:mm:ss.SSSZ";
+export const MILLISECONDS_PER_DAY = 86400000;
